7b9cdade47b113abc370454acdcd08e8fb04edad
adb-sh <git@adb.sh>
2022-12-02 03:14:56 +0100

    add About page

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import Component from "../../../dynaticjs/Component.js";
+
+export default class Home extends Component {
+  render() {
+    return `
+      <div class="home">
+        <h2>About</h2>
+        <p>
+          DynaticJS is a simple reactive runtime framework.
+        </p>
+      </div>
+    `;
+  }
+}
